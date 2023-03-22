0baeda36ab247dad095878965dc79cfbfa6ae564
KacperZiolek <118622850+KacperZiolek@users.noreply.github.com>
2023-03-22 15:33:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    String str = "HelloWorld";
+    for (int i = 0; i < str.length(); i++) {
+      System.out.println(str.charAt(i));
+    }
+  }
+}
